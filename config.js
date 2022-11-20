bb59334f2b8929ca99fae42174b1ec5e9f83b2ed
oofioo <102270626+oofioo@users.noreply.github.com>
2022-11-20 23:25:56 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const fs = require("fs")
 const chalk = require("chalk")
 
 global.available = true
-global.autoReadGc = true
+global.autoReadGc = false
 global.autoReadAll = false
 global.antitags = true
 
@@ -59,18 +59,18 @@ global.ntilink = []
 
 global.mess = {
     jobdone: 'Job done...',
-    useradmin: 'Sorry, only *Group Admins* can use this command *Baka*!',
+    useradmin: 'Sorry, only *Group Admins* can use this command, *idiot* !',
     botadmin: 'Sorry, i cant execute this command without being an *Admin* of this group.',
-    botowner: 'Only my *Owner* can use this command, Baka!',
-    grouponly: 'This command is only made for *Groups*, Baka!',
-    privateonly: 'This command is only made for *Private Chat*, Baka!',
+    botowner: 'Only my *Owner* can use this command, stupid!',
+    grouponly: 'This command is only made for *Groups*, idiot!',
+    privateonly: 'This command is only made for *Private Chat*, idiot!',
     botonly: 'Only the *Bot itself* can use this command!',
     waiting: 'Chotto Matte...',
-    nolink: 'Please provide me *link*, Baka!',
-    error: 'An error occurd!',
+    nolink: 'Please provide me a *link* !',
+    error: 'An error occured!',
     banned: 'You are *Banned* fron using commands!',
     bangc: 'This Group is *Banned* from using Commands!',
-    nonsfw: 'Dont be a pervert Baka! This is not a NSFW enabled group!'
+    nonsfw: 'Dont be a pervert, idiot! This is not a NSFW enabled group!'
     
 }
 
